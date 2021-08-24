47e5d94c0446938b59f579a6377c71c65410af7a
Philip Degarmo <aclysma@gmail.com>
2021-08-23 20:58:45 -0700

    Initial window setup with egui/bevy

--- m3/src/lib.rs
+++ m3/src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+
+pub fn run() {
+    let mut window_descriptor = WindowDescriptor::default();
+    window_descriptor.title = "m3".to_string();
+
+    App::build()
+        .insert_resource(window_descriptor)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        .add_system(ui_example.system())
+        .run();
+}
+
+fn ui_example(egui_context: ResMut<EguiContext>) {
+    egui::SidePanel::left("left_panel").show(egui_context.ctx(), |ui| {
+        ui.label("hi");
+    });
 }

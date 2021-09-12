e6f59891c14bf4073bfd533737666697d9aec5d4
Philip Degarmo <aclysma@gmail.com>
2021-09-11 17:50:20 -0700

    laptop WIP

---
+++ conduit/src/main.rs
@@ -0,0 +1,14 @@
+
+
+
+
+fn main() {
+    // Setup logging
+    env_logger::Builder::default()
+        .write_style(env_logger::WriteStyle::Always)
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
+
+    conduit::run();
+}

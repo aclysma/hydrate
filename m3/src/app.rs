d7b6c4d84b0b060e72dfe4c994d91e9546878d84
Philip Degarmo <aclysma@gmail.com>
2022-10-09 01:06:53 -0700

    WIP

--- m3/src/app.rs
+++ m3/src/app.rs
@@ -7,6 +7,8 @@ pub struct AppState {
     // pub instance_obj: ObjectId,
 
     pub redock_windows: bool,
+    pub splitter_size1: f32,
+    pub splitter_size2: f32,
 
     pub test_data_nexdb: TestData,
     //TODO: New DB type here, update draw_2_pane_view to call draw_inspector with new data
@@ -16,6 +18,8 @@ impl AppState {
     pub fn new(test_data: TestData) -> Self {
         AppState {
             redock_windows: true,
+            splitter_size1: 100.0,
+            splitter_size2: 100.0,
             test_data_nexdb: test_data
         }
     }
--- m3/src/draw_dockspace.rs
+++ m3/src/draw_dockspace.rs
@@ -22,10 +22,23 @@ pub fn draw_assets_dockspace(
     app_state: &mut AppState,
 ) {
     unsafe {
-        let mut work_size = ImVec2::new(1416.0, 285.0);
+        //let mut work_size = ImVec2::new(1416.0, 285.0);
+        //let mut work_size = ImVec2::new(0.0, 0.0);
         //is::igGetContentRegionAvail(&mut work_size);
         //is::igGetWindowContentRegionMax(&mut work_size);
         //is::igGetWindowSize(&mut work_size);
+
+
+
+        let window = imgui::sys::igGetCurrentWindow();
+        let mut work_size = (*window).Size;
+        //work_size.x = 400.0;
+        work_size.y -= 22.0;
+        //let work_pos = (*window).WorkRect.Min.clone();
+        //let mut work_size = (*window).WorkRect.Max.clone();
+        //work_size.x -= work_pos.x;
+        //work_size.y -= work_pos.y;
+
         println!("work size {:?}", work_size);
 
         // let window = imgui::sys::igGetCurrentWindow();
@@ -44,7 +57,7 @@ pub fn draw_assets_dockspace(
             is::igDockBuilderAddNode(assets_main, is::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_DockSpace | imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoDocking| imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoDockingSplitMe);
 
             is::igDockBuilderSetNodeSize(root_assets_dockspace_id, work_size);
-            let assets_left = is::igDockBuilderSplitNode(assets_main, is::ImGuiDir_Left, 0.2, std::ptr::null_mut(), &mut assets_main);
+            let assets_left = is::igDockBuilderSplitNode(assets_main, is::ImGuiDir_Left, 1.0, std::ptr::null_mut(), &mut assets_main);
 
             is::igDockBuilderDockWindow(ImString::new(WINDOW_NAME_ASSETS_LEFT).as_ptr(), assets_left);
             is::igDockBuilderDockWindow(ImString::new(WINDOW_NAME_ASSETS_RIGHT).as_ptr(), assets_main);
@@ -54,10 +67,61 @@ pub fn draw_assets_dockspace(
             (*imgui::sys::igDockBuilderGetNode(assets_main)).LocalFlags |= imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoTabBar;// | imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoDocking | imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoDockingSplitMe | imgui::sys::ImGuiDockNodeFlagsPrivate__ImGuiDockNodeFlags_NoDockingSplitOther;
 
             is::igDockBuilderFinish(root_assets_dockspace_id);
-
         }
     }
 }
+
+// bool Splitter(bool split_vertically, float thickness, float* size1, float* size2, float min_size1, float min_size2, float splitter_long_axis_size = -1.0f)
+// {
+// using namespace ImGui;
+// ImGuiContext& g = *GImGui;
+// ImGuiWindow* window = g.CurrentWindow;
+// ImGuiID id = window->GetID("##Splitter");
+// ImRect bb;
+// bb.Min = window->DC.CursorPos + (split_vertically ? ImVec2(*size1, 0.0f) : ImVec2(0.0f, *size1));
+// bb.Max = bb.Min + CalcItemSize(split_vertically ? ImVec2(thickness, splitter_long_axis_size) : ImVec2(splitter_long_axis_size, thickness), 0.0f, 0.0f);
+// return SplitterBehavior(id, bb, split_vertically ? ImGuiAxis_X : ImGuiAxis_Y, size1, size2, min_size1, min_size2, 0.0f);
+// }
+
+fn splitter(vertical: bool, thickness: f32, size1: &mut f32, size2: &mut f32, min_size1: f32, min_size2: f32) {
+    unsafe {
+        let splitter_long_axis_size = -1.0;
+        let window = is::igGetCurrentWindow();
+        let id = is::igGetID_Str(CString::new("##Splitter").unwrap().as_ptr());
+        let mut bb = is::ImRect {
+            Min: ImVec2::zero(),
+            Max: ImVec2::zero()
+        };
+        let add_to_min = if vertical {
+            is::ImVec2::new(*size1, 0.0)
+        } else {
+            is::ImVec2::new(0.0, *size1)
+        };
+
+        let calc_item_size_in = if vertical {
+            is::ImVec2::new(thickness, splitter_long_axis_size)
+        } else {
+            is::ImVec2::new(splitter_long_axis_size, thickness)
+        };
+        let mut calc_item_size_out = ImVec2::zero();
+        is::igCalcItemSize(&mut calc_item_size_out as _, calc_item_size_in, 0.0, 0.0);
+
+        let cursor_pos = (*window).DC.CursorPos;
+        bb.Min = ImVec2::new(cursor_pos.x + add_to_min.x, cursor_pos.y + add_to_min.y);
+        bb.Max = ImVec2::new(bb.Min.x + calc_item_size_out.x, bb.Min.y + calc_item_size_out.y);
+
+        let axis = if vertical {
+            is::ImGuiAxis_ImGuiAxis_X
+        } else {
+            is::ImGuiAxis_ImGuiAxis_Y
+        };
+
+        is::igSplitterBehavior(bb, id, axis, size1, size2, min_size1, min_size2, 0.0, 0.0);
+
+
+    }
+}
+
 
 pub fn draw_dockspace(
     ui: &imgui::Ui,
@@ -109,16 +173,40 @@ pub fn draw_dockspace(
         window_token.end();
     }
 
+    unsafe {
+        //imgui::sys::igPushStyleVar_Float(imgui::sys::ImGuiStyleVar_WindowRounding as _, 0.0);
+        //imgui::sys::igPushStyleVar_Float(imgui::sys::ImGuiStyleVar_WindowBorderSize as _, 0.0);
+        is::igPushStyleVar_Vec2(
+            is::ImGuiStyleVar_WindowPadding as _,
+            ImVec2::new(0.0, 0.0),
+        );
+    }
+
     let window_token = imgui::Window::new(&ImString::new(WINDOW_NAME_ASSETS))
         //.position([550.0, 100.0], imgui::Condition::Once)
-        //.size([300.0, 400.0], imgui::Condition::Once)
+        .size([300.0, 400.0], imgui::Condition::Once)
+        .flags(imgui::WindowFlags::NO_COLLAPSE)
         .begin(ui);
+
+    unsafe {
+        is::igPopStyleVar(1);
+    }
 
     if let Some(window_token) = window_token {
         //ui.text(im_str!("assets"));
+
+        //
+        // SPLITTER APPROACH
+        //
+        //let mut size1 = 200.0;
+        //let mut size2 = 200.0;
+        //let mut min_size1 = 10.0;
+        //let mut min_size2 = 10.0;
+        //splitter(true, 1.0, &mut app_state.splitter_size1, &mut app_state.splitter_size2, min_size1, min_size2);
+
+
+
         draw_assets_dockspace(ui, app_state);
-
-
 
 
         let inner_window_token = imgui::Window::new(&ImString::new(WINDOW_NAME_ASSETS_LEFT))

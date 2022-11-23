26207288bc3af622aa17218500437b0a1b454690
Philip Degarmo <aclysma@gmail.com>
2022-11-22 22:58:47 -0800

    Fix nexdb examples, move data into per-example subdirs

--- nexdb/examples/file_system_data_source.rs
+++ nexdb/examples/file_system_data_source.rs
@@ -1,4 +1,4 @@
-use nexdb::{FileSystemTreeDataSource, ObjectPath, SchemaCacheSingleFile, SchemaSet, UndoStack};
+use nexdb::{EditContextKey, EditorModel, FileSystemTreeDataSource, ObjectPath, SchemaCacheSingleFile, SchemaSet, UndoStack};
 use std::path::PathBuf;
 use std::sync::Arc;
 use nexdb::edit_context::EditContext;
@@ -6,11 +6,11 @@ use nexdb::edit_context::EditContext;
 pub fn main() {
     let schema_cache_path = PathBuf::from(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/examples/fs_data_source/schema_cache.json"
+        "/examples/data/file_system_data_source/schema_cache.json"
     ));
     let data_source_path = PathBuf::from(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/examples/fs_data_source"
+        "/examples/data/file_system_data_source"
     ));
 
     let mut schema_set = SchemaSet::default();
@@ -18,12 +18,12 @@ pub fn main() {
     let cache_str = std::fs::read_to_string(&schema_cache_path).unwrap();
     SchemaCacheSingleFile::load_string(&mut schema_set, &cache_str);
 
-    let undo_stack = UndoStack::default();
-    let mut edit_context = EditContext::new(Arc::new(schema_set), &undo_stack);
+    let mut editor_model = EditorModel::new(Arc::new(schema_set));
+    let edit_context = editor_model.root_edit_context_mut();
 
-    let fs = FileSystemTreeDataSource::new(data_source_path, ObjectPath::root(), &mut edit_context);
+    let fs = FileSystemTreeDataSource::new(data_source_path, ObjectPath::root(), edit_context);
 
-    println!("file_states {:#?}", fs.file_states());
+    //println!("file_states {:#?}", fs.file_states());
     for (object_id, object_info) in edit_context.objects() {
         println!("{:?} {:?}", object_id, object_info.object_location());
         println!("  on disk: {:?}", fs.location_to_file_system_path(object_info.object_location()));
--- nexdb/examples/simple_example.rs
+++ nexdb/examples/simple_example.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 
 use nexdb::edit_context::EditContext;
 use nexdb::*;
+use nexdb::json::TreeSourceDataStorageJsonSingleFile;
 
 fn main() {
     env_logger::Builder::default()
@@ -10,7 +11,7 @@ fn main() {
         .filter_level(log::LevelFilter::Debug)
         .init();
 
-    let path = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/schema"));
+    let path = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/data/simple_example/schema"));
     let default_location = ObjectLocation::new(ObjectSourceId::new(), ObjectPath::new("test.nxt"));
 
     let (schema_cache, data) = {
@@ -21,8 +22,8 @@ fn main() {
         schema_set.add_linked_types(linker).unwrap();
         let schema_set = Arc::new(schema_set);
 
-        let undo_stack = UndoStack::default();
-        let mut edit_context = EditContext::new(schema_set.clone(), &undo_stack);
+        let mut edit_model = EditorModel::new(schema_set.clone());
+        let mut edit_context = edit_model.root_edit_context_mut();
         //linker.finish();
 
         let vec3_type = edit_context
@@ -32,7 +33,7 @@ fn main() {
             .unwrap()
             .clone();
 
-        let vec3_obj = edit_context.new_object(default_location.clone(), &vec3_type);
+        let vec3_obj = edit_context.new_object(&default_location, &vec3_type);
         edit_context.set_property_override(vec3_obj, "x", Value::F32(10.0));
         edit_context.set_property_override(vec3_obj, "y", Value::F32(20.0));
         edit_context.set_property_override(vec3_obj, "z", Value::F32(30.0));
@@ -43,7 +44,7 @@ fn main() {
             .as_record()
             .unwrap()
             .clone();
-        let aabb_obj = edit_context.new_object(default_location.clone(), &aabb_type);
+        let aabb_obj = edit_context.new_object(&default_location, &aabb_type);
         edit_context.set_property_override(aabb_obj, "min.x", Value::F32(10.0));
         edit_context.set_property_override(aabb_obj, "min.y", Value::F32(20.0));
         edit_context.set_property_override(aabb_obj, "min.z", Value::F32(30.0));
@@ -58,7 +59,7 @@ fn main() {
             .as_record()
             .unwrap()
             .clone();
-        let dyn_array_obj = edit_context.new_object(default_location.clone(), &dyn_array_type);
+        let dyn_array_obj = edit_context.new_object(&default_location, &dyn_array_type);
 
         let element1 = edit_context.add_dynamic_array_override(dyn_array_obj, "dyn_array");
         let _element2 = edit_context.add_dynamic_array_override(dyn_array_obj, "dyn_array");
@@ -77,7 +78,7 @@ fn main() {
 
         let schema_cache = SchemaCacheSingleFile::store_string(edit_context.schema_set());
         let object_ids: Vec<_> = edit_context.all_objects().copied().collect();
-        let data = DataStorageJsonSingleFile::store_string(&edit_context, &object_ids);
+        let data = TreeSourceDataStorageJsonSingleFile::store_objects_to_string(&edit_context, &object_ids);
 
         println!("Schema Cache: {}", schema_cache);
         println!("Data: {}", data);
@@ -93,12 +94,13 @@ fn main() {
         schema_set.add_linked_types(linker2).unwrap();
         let schema_set = Arc::new(schema_set);
 
-        let undo_stack = UndoStack::default();
-        let mut edit_context2 = EditContext::new(schema_set.clone(), &undo_stack);
 
-        DataStorageJsonSingleFile::load_string(&mut edit_context2, default_location.clone(), &data);
+        let mut edit_model2 = EditorModel::new(schema_set.clone());
+        let mut edit_context2 = edit_model2.root_edit_context_mut();
+
+        TreeSourceDataStorageJsonSingleFile::load_objects_from_string(&mut edit_context2, default_location.clone(), &data);
         let object_ids: Vec<_> = edit_context2.all_objects().copied().collect();
-        let data2 = DataStorageJsonSingleFile::store_string(&edit_context2, &object_ids);
+        let data2 = TreeSourceDataStorageJsonSingleFile::store_objects_to_string(&edit_context2, &object_ids);
         println!("Data2: {}", data2);
         assert_eq!(data, data2);
 
@@ -112,12 +114,12 @@ fn main() {
         SchemaCacheSingleFile::load_string(&mut schema_set, &schema_cache);
         let schema_set = Arc::new(schema_set);
 
-        let undo_stack = UndoStack::default();
-        let mut edit_context3 = EditContext::new(schema_set.clone(), &undo_stack);
+        let mut edit_model3 = EditorModel::new(schema_set.clone());
+        let mut edit_context3 = edit_model3.root_edit_context_mut();
 
-        DataStorageJsonSingleFile::load_string(&mut edit_context3, default_location, &data);
+        TreeSourceDataStorageJsonSingleFile::load_objects_from_string(&mut edit_context3, default_location, &data);
         let object_ids: Vec<_> = edit_context3.all_objects().copied().collect();
-        let data3 = DataStorageJsonSingleFile::store_string(&edit_context3, &object_ids);
+        let data3 = TreeSourceDataStorageJsonSingleFile::store_objects_to_string(&edit_context3, &object_ids);
         println!("---------------------");
         println!("Data3: {}", data3);
         assert_eq!(data, data3);
@@ -128,13 +130,13 @@ fn main() {
 
     let data_file_path = PathBuf::from(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/examples/data/data_file_out.json"
+        "/examples/data/simple_example/data_file_out.json"
     ));
     std::fs::write(data_file_path, data).unwrap();
 
     let schema_cache_file_path = PathBuf::from(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/examples/schema_cache/schema_cache_file_out.json"
+        "/examples/data/simple_example/schema_cache/schema_cache_file_out.json"
     ));
     std::fs::write(schema_cache_file_path, schema_cache).unwrap();
 }

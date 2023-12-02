6d0d81f8b38855b5bf33d45504255176845ab710
Philip Degarmo <aclysma@gmail.com>
2023-12-01 22:42:44 -0800

    remove old editor

--- hydrate-editor/src/ui/modals/mod.rs
+++
@@ -1,8 +0,0 @@
-mod confirm_quit_without_saving_modal;
-pub use confirm_quit_without_saving_modal::*;
-
-mod import_files_modal;
-pub use import_files_modal::*;
-
-mod new_asset_modal;
-pub use new_asset_modal::*;
--- hydrate-editor/src/ui/windows/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod assets_window;
-pub mod external_references_window;
-pub mod properties_window;
